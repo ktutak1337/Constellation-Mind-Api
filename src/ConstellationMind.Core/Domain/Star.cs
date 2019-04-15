78452f54d4809618c541e90f5d2af894e312a445
KTutak <ktutak1337@gmail.com>
2019-04-16 01:57:22 +0200

    Create simple domain models (Code) - Created Constellation model - Created Player model - Created PlayerScore model - Created Scoreboard model - Created Star model - Created IIdentity interface - Added reference to ConstellationMind.Core project

---
+++ src/ConstellationMind.Core/Domain/Star.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace ConstellationMind.Core.Domain
+{
+    public class Star
+    {
+        #region Properties
+        public string Name { get; protected set; }
+        public string Constellation { get; protected set; }
+        public double Ra { get; protected set; }
+        public double Dec { get; protected set; }
+        public double Brightness { get; protected set; }
+
+        #endregion
+
+        #region Constructors
+        protected Star() {}
+
+        public Star(string name, string constellation, double ra, double dec, double brightness)
+        {
+            Name = name;
+            Constellation = constellation;
+            Ra = ra;
+            Dec = dec;
+            Brightness = brightness;
+        }
+
+        #endregion
+
+        #region Methods
+        public static Star Create(string name, string constellation, double ra, double dec, double brightness)
+            => new Star(name, constellation, ra, dec, brightness);
+
+        #endregion    
+    }
+}
