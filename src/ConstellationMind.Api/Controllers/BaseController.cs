e60f779329b690752d6539a1f9ff9a1a2c54b12c
KTutak <ktutak1337@gmail.com>
2020-02-19 03:46:11 +0100

    Add comments for swagger documentation in Accounts Controller (Code) - Added header media type definition for request responses in BaseController class - Added required comments for swagger documentation in AccountsController class

--- src/ConstellationMind.Api/Controllers/BaseController.cs
+++ src/ConstellationMind.Api/Controllers/BaseController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ConstellationMind.Api.Controllers
 {
     [Route("api/[controller]")]
+    [Produces("application/json")]
     [ApiController]
     public abstract class BaseController : ControllerBase
     {
