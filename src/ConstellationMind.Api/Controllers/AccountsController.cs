c6872cb66d114df3b266936bc4035c1691f5f945
KTutak <ktutak1337@gmail.com>
2020-02-27 21:13:22 +0100

    Add comments for swagger documentation in Tokens Controller (Code) - Added required comments for swagger documentation in Tokens Controller class - Fixed comments for swagger documentation in Accounts and Players Controller

--- src/ConstellationMind.Api/Controllers/AccountsController.cs
+++ src/ConstellationMind.Api/Controllers/AccountsController.cs
@@ -23,11 +23,11 @@ namespace ConstellationMind.Api.Controllers
         }
 
         /// <summary>
-        /// Returns a unique ``ID`` for the currently logged player.
+        /// Returns a unique `ID` for the currently logged player.
         /// </summary>
-        /// <response code="200">Returns a unique ``ID`` for a single specific user. The HTTP ``200 [OK]`` response code indicates that the request has succeeded.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]`` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="200">Returns a unique `ID` for a single specific user. The HTTP `200 [OK]` response code indicates that the request has succeeded.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpGet("me")]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -38,9 +38,9 @@ namespace ConstellationMind.Api.Controllers
         /// <summary>
         /// Returns specific details for the currently logged player.
         /// </summary>
-        /// <response code="200">Returns details information for a single specific user. The HTTP ``200 [OK]`` response code indicates that the request has succeeded.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]`` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="200">Returns details information for a single specific user. The HTTP `200 [OK]` response code indicates that the request has succeeded.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpGet("me/details")]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -55,9 +55,9 @@ namespace ConstellationMind.Api.Controllers
         /// <summary>
         /// Creates a new player account.
         /// </summary>
-        /// <response code="201">Returns the newly created player. The HTTP ``201 [Created]`` response code indicates that the request has succeeded.</response>
-        /// <response code="400">The HTTP ``400 [Bad Request]`` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="201">Returns the newly created player. The HTTP `201 [Created]` response code indicates that the request has succeeded.</response>
+        /// <response code="400">The HTTP `400 [Bad Request]` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpPost("sign-up")]
         [Consumes("application/json")]
         [ProducesResponseType(StatusCodes.Status201Created)]
@@ -74,9 +74,9 @@ namespace ConstellationMind.Api.Controllers
         /// <summary>
         /// Sign in to the Constellation Mind.
         /// </summary>
-        /// <response code="200">The HTTP ``200 [OK]`` response code indicates that the request has succeeded.</response>
-        /// <response code="400">The HTTP ``400 [Bad Request]`` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="200">The HTTP `200 [OK]` response code indicates that the request has succeeded.</response>
+        /// <response code="400">The HTTP `400 [Bad Request]` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpPost("sign-in")]
         [Consumes("application/json")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -89,10 +89,10 @@ namespace ConstellationMind.Api.Controllers
         /// <summary>
         /// Changes player password.
         /// </summary>
-        /// <response code="204">The HTTP ``204 [No Content]`` response code indicates that the server successfully processed the request and is not returning any content.</response>
-        /// <response code="400">The HTTP ``400 [Bad Request]`` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]`` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="204">The HTTP `204 [No Content]` response code indicates that the server successfully processed the request and is not returning any content.</response>
+        /// <response code="400">The HTTP `400 [Bad Request]` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpPut("me/password/change")]
         [Consumes("application/json")] 
         [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -113,10 +113,10 @@ namespace ConstellationMind.Api.Controllers
         /// Deactivates or activates a specific player account.
         /// </summary>
         /// <param name="id">The unique player identifier.</param>
-        /// <response code="204">The HTTP ``204 [No Content]`` response code indicates that the server successfully processed the request and is not returning any content.</response>
-        /// <response code="400">The HTTP ``400 [Bad Request]`` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]``response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="204">The HTTP `204 [No Content]` response code indicates that the server successfully processed the request and is not returning any content.</response>
+        /// <response code="400">The HTTP `400 [Bad Request]` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpPut("{id}/status")]
         [Consumes("application/json")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -137,11 +137,11 @@ namespace ConstellationMind.Api.Controllers
         /// Deletes a specific player account.
         /// </summary>
         /// <param name="id">The unique player identifier.</param>
-        /// <response code="204">The HTTP ``204 [No Content]`` response code indicates that the server successfully processed the request and is not returning any content.</response>
-        /// <response code="400">The HTTP ``400 [Bad Request]`` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]``response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="403">The HTTP ``403 [Forbidden]`` response code indicates that the server understood the request but refuses to authorize it.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="204">The HTTP `204 [No Content]` response code indicates that the server successfully processed the request and is not returning any content.</response>
+        /// <response code="400">The HTTP `400 [Bad Request]` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]`response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="403">The HTTP `403 [Forbidden]` response code indicates that the server understood the request but refuses to authorize it.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
--- src/ConstellationMind.Api/Controllers/PlayersController.cs
+++ src/ConstellationMind.Api/Controllers/PlayersController.cs
@@ -21,12 +21,12 @@ namespace ConstellationMind.Api.Controllers
         }
 
         /// <summary>
-        /// Returns a single player by ``ID``.
+        /// Returns a single player by `ID`.
         /// </summary>
         /// <param name="id">The unique player identifier.</param>
-        /// <response code="200">Returns a unique ``ID`` for a single specific user. The HTTP ``200 [OK]`` response code indicates that the request has succeeded.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]`` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="200">Returns a unique `ID` for a single specific user. The HTTP `200 [OK]` response code indicates that the request has succeeded.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -36,9 +36,9 @@ namespace ConstellationMind.Api.Controllers
         /// <summary>
         /// Returns list of players.
         /// </summary>
-        /// <response code="200">Returns a unique ``ID`` for a single specific user. The HTTP ``200 [OK]`` response code indicates that the request has succeeded.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]`` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="200">Returns a unique `ID` for a single specific user. The HTTP `200 [OK]` response code indicates that the request has succeeded.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]` response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -49,10 +49,10 @@ namespace ConstellationMind.Api.Controllers
         /// Updates the number of points for a specific player.
         /// </summary>
         /// <param name="id">The unique player identifier.</param>
-        /// <response code="204">The HTTP ``204 [No Content]`` response code indicates that the server successfully processed the request and is not returning any content.</response>
-        /// <response code="400">The HTTP ``400 [Bad Request]`` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]``response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="204">The HTTP `204 [No Content]` response code indicates that the server successfully processed the request and is not returning any content.</response>
+        /// <response code="400">The HTTP `400 [Bad Request]` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]`response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpPut("{id}/points")]
         [Consumes("application/json")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
@@ -72,10 +72,10 @@ namespace ConstellationMind.Api.Controllers
         /// Updates the details of a specific player.
         /// </summary>
         /// <param name="id">The unique player identifier.</param>
-        /// <response code="204">The HTTP ``204 [No Content]`` response code indicates that the server successfully processed the request and is not returning any content.</response>
-        /// <response code="400">The HTTP ``400 [Bad Request]`` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
-        /// <response code="401">The HTTP ``401 [Unauthorized]``response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
-        /// <response code="404">The HTTP ``404 [Not Found]`` response code indicates that the server can not find the requested resource.</response>
+        /// <response code="204">The HTTP `204 [No Content]` response code indicates that the server successfully processed the request and is not returning any content.</response>
+        /// <response code="400">The HTTP `400 [Bad Request]` response code indicates that the server cannot or will not process the request due to something that is perceived to be a client error.</response>
+        /// <response code="401">The HTTP `401 [Unauthorized]`response code indicates that the request has not been applied because it lacks valid authentication credentials for the target resource.</response>
+        /// <response code="404">The HTTP `404 [Not Found]` response code indicates that the server can not find the requested resource.</response>
         [HttpPut("{id}/details")]
         [Consumes("application/json")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
